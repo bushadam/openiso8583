837421898edb43f268417b16af5f175f16214e8c
John Oxley <john.oxley@gmail.com>
2014-02-27 10:38:01 +0200

    Initial Import

---
+++ OpenIso8583Net.Tests/Iso8583PostTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenIso8583Net;
+
+namespace OpenIso8583Net.Tests
+{
+    /// <summary>
+    ///   Summary description for Iso8583PostTests
+    /// </summary>
+    [TestClass]
+    public class Iso8583PostTests
+    {
+        ///<summary>
+        ///  Gets or sets the test context which provides
+        ///  information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestIso8583PostToMsg()
+        {
+            var msg = new Iso8583Post();
+            msg.MessageType = 0x200;
+            msg[3] = "000000";
+            msg.Private[2] = "hello";
+            var actual = msg.ToMsg();
+
+            var mtid = Encoding.ASCII.GetBytes("0200");
+
+            var bitmap = new Bitmap();
+            bitmap[3] = true;
+            bitmap[127] = true;
+            var primaryBitmap = bitmap.ToMsg();
+            var primaryMessageContent = Encoding.ASCII.GetBytes("000000");
+
+            bitmap = new Bitmap();
+            bitmap[2] = true;
+            var privateBitmap = bitmap.ToMsg();
+            var privateContent = Encoding.ASCII.GetBytes("05hello");
+            var privateLength = privateBitmap.Length + privateContent.Length;
+            var privateMessage = new byte[privateLength];
+            Array.Copy(privateBitmap, privateMessage, privateBitmap.Length);
+            Array.Copy(privateContent, 0, privateMessage, privateBitmap.Length, privateContent.Length);
+            var privateMessageLengthHeader = Encoding.ASCII.GetBytes(privateLength.ToString().PadLeft(6, '0'));
+
+            var messageLength = 4 + primaryBitmap.Length + 6 + 6 + privateMessage.Length;
+
+            var message = new byte[messageLength];
+            var offset = 0;
+            Array.Copy(mtid, 0, message, offset, mtid.Length);
+            offset += mtid.Length;
+
+            Array.Copy(primaryBitmap, 0, message, offset, primaryBitmap.Length);
+            offset += primaryBitmap.Length;
+
+            Array.Copy(primaryMessageContent, 0, message, offset, primaryMessageContent.Length);
+            offset += primaryMessageContent.Length;
+
+            Array.Copy(privateMessageLengthHeader, 0, message, offset, privateMessageLengthHeader.Length);
+            offset += privateMessageLengthHeader.Length;
+
+            Array.Copy(privateMessage, 0, message, offset, privateMessage.Length);
+
+            Assert.AreEqual(messageLength, msg.PackedLength, "Message length not equal");
+
+            var equals = true;
+            for (var i = 0; i < messageLength; i++)
+                equals &= message[i] == actual[i];
+
+            Assert.AreEqual(true, equals, "Messages not equal");
+        }
+
+        [TestMethod]
+        public void TestIso8583PostUnpack()
+        {
+            var mtid = Encoding.ASCII.GetBytes("0200");
+
+            var bitmap = new Bitmap();
+            bitmap[3] = true;
+            bitmap[127] = true;
+            var primaryBitmap = bitmap.ToMsg();
+            var primaryMessageContent = Encoding.ASCII.GetBytes("000000");
+
+            bitmap = new Bitmap();
+            bitmap[2] = true;
+            var privateBitmap = bitmap.ToMsg();
+            var privateContent = Encoding.ASCII.GetBytes("05hello");
+            var privateLength = privateBitmap.Length + privateContent.Length;
+            var privateMessage = new byte[privateLength];
+            Array.Copy(privateBitmap, privateMessage, privateBitmap.Length);
+            Array.Copy(privateContent, 0, privateMessage, privateBitmap.Length, privateContent.Length);
+            var privateMessageLengthHeader = Encoding.ASCII.GetBytes(privateLength.ToString().PadLeft(6, '0'));
+
+            var messageLength = 4 + primaryBitmap.Length + 6 + 6 + privateMessage.Length;
+
+            var message = new byte[messageLength];
+            var offset = 0;
+            Array.Copy(mtid, 0, message, offset, mtid.Length);
+            offset += mtid.Length;
+
+            Array.Copy(primaryBitmap, 0, message, offset, primaryBitmap.Length);
+            offset += primaryBitmap.Length;
+
+            Array.Copy(primaryMessageContent, 0, message, offset, primaryMessageContent.Length);
+            offset += primaryMessageContent.Length;
+
+            Array.Copy(privateMessageLengthHeader, 0, message, offset, privateMessageLengthHeader.Length);
+            offset += privateMessageLengthHeader.Length;
+
+            Array.Copy(privateMessage, 0, message, offset, privateMessage.Length);
+
+            var msg = new Iso8583Post();
+            msg.Unpack(message, 0);
+
+            Assert.AreEqual(message.Length, msg.PackedLength);
+            Assert.AreEqual("000000", msg[3]);
+            Assert.AreEqual("hello", msg.Private[2]);
+        }
+
+        [TestMethod]
+        public void TestDontKnow()
+        {
+            var msg = new Iso8583Post();
+            msg.MessageType = Iso8583.MsgType._0200_TRAN_REQ;
+            msg[3] = "270000";
+            msg.TransactionAmount = 400;
+            msg.TransmissionDateTime.SetNow();
+            msg[11] = "123456";
+            msg[12] = "151518";
+            msg[13] = "1212";
+            msg[22] = "012";
+            msg[25] = "00";
+            msg[26] = "12";
+            msg[32] = "588892";
+            msg[33] = "123456";
+            msg[37] = "123456789123";
+            msg[41] = "21458796";
+            msg[42] = "100200300400500";
+            msg[43] = new string('x', 40);
+            msg[48] = "A";
+            msg[49] = "716";
+            msg[100] = "123456";
+            msg[102] = "9012273811";
+            msg[103] = "010203040506";
+            msg[123] = "100111100130119";
+
+            msg.Private[Field127.Bit._002_SWITCH_KEY] = DateTime.Now.ToString("yyyyMMDDHHmmss");
+
+#pragma warning disable 168
+            var data = msg.ToMsg();
+#pragma warning restore 168
+        }
+    }
+}
---
+++ OpenIso8583Net/Utils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace OpenIso8583Net
+{
+    /// <summary>
+    /// Utilities class with helper functions
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Returns the luhn digit for a given PAN
+        /// </summary>
+        /// <param name="pan">PAN missing the luhn check digit</param>
+        /// <returns>Luhn check digit</returns>
+        public static string GetLuhn(string pan)
+        {
+            int sum = 0;
+
+            bool alternate = true;
+            for (int i = pan.Length - 1; i >= 0; i--)
+            {
+                int num = int.Parse(pan[i].ToString());
+
+                if (alternate)
+                {
+                    num *= 2;
+                    if (num > 9)
+                        num = num - 9;
+                }
+
+                sum += num;
+                alternate = !alternate;
+            }
+
+            int luhnDigit = 10 - (sum % 10);
+            if (luhnDigit == 10)
+                luhnDigit = 0;
+
+            return luhnDigit.ToString();
+        }
+
+        /// <summary>
+        /// Checks that the luhn check digit is valid
+        /// </summary>
+        /// <param name="pan">PAN to validate</param>
+        /// <returns>true if valid, false otherwise</returns>
+        public static bool IsValidPAN(String pan)
+        {
+            string luhn = GetLuhn(pan.Substring(0, pan.Length - 1));
+            return luhn == pan.Substring(pan.Length - 1);
+        }
+
+        /// <summary>
+        /// PCI DSS PAN mask. For strings longer than 10 chars masks characters [6..Length-4] 
+        /// by character 'x'; otherwise returns the pan parameter unchanged.
+        /// </summary>
+        /// <param name="pan">a PAN string</param>
+        /// <returns>a masked PAN string</returns>
+        public static string MaskPan(string pan)
+        {
+            if (pan == null)
+                return null;
+
+            const int frontLength = 6;
+            const int endLength = 4;
+            const int unmaskedLength = frontLength + endLength;
+
+            var totalLength = pan.Length;
+
+            if (totalLength <= unmaskedLength)
+                return pan;
+
+            return
+                new StringBuilder(totalLength, totalLength)
+                    .Append(pan.Substring(0, frontLength)) // front
+                    .Append(new string('x', totalLength - unmaskedLength))  // mask
+                    .Append(pan.Substring((totalLength - endLength), endLength)) // end
+                    .ToString();
+        }
+    }
+}
